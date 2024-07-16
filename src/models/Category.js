b534840e0e8d85763b4acdea7f7ca0251710c19f
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-07-16 23:12:37 +0700

    category -done

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		description: {
+			type: String,
+		},
+	},
+	{ timestamps: true, versionKey: false }
+);
+
+export default mongoose.model("Product", productSchema);
