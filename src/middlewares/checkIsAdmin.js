b45eea8d0807e787b9bedd8f2c25b3ec3c21a78b
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-07-30 15:05:52 +0700

    cart

--- src/middlewares/checkIsAdmin.js
+++ src/middlewares/checkIsAdmin.js
@@ -5,7 +5,6 @@ export const checkIsAdmin = async (req, res, next) => {
 				message: "Unthorized",
 			});
 		}
-		console.log("admin");
 		next();
 	} catch (error) {
 		return res.status(401).json({
