f829203bebb4fcc8c470a1c91048da985c435358
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-07-30 15:35:15 +0700

    cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,5 +1,5 @@
 import Cart from "../models/Cart.js";
-import User from "../models/User";
+import Product from "../models/Product.js";
 
 export const getCart = async (req, res, next) => {
 	try {
@@ -13,15 +13,29 @@ export const addToCart = async (req, res, next) => {
 	try {
 		const userId = req.user._id;
 		const { productId, quantity } = req.body;
+		const product = await Product.findById(productId);
 		// Nếu người dùng chưa có cart thì tạo cart, nếu có rồi thì thêm vào cart.
 
-		const cart = Cart.findOne({ userId });
-		const productIndex = cart.findIndex((item) => item.product == productId);
-		if (productIndex == -1) {
+		let cart = await Cart.findOne({ userId });
+		if (!cart) cart = new Cart({ userId, products: [], totalPrice: 0 });
+
+		console.log(cart);
+		const productIndex = cart.products.findIndex((item) => item.product == productId);
+		if (productIndex === -1) {
+			// Nếu sản phẩm chưa có trong cart.products thì push sản phẩm vào cart.product kèm theo quantity
 			cart.products.push({ product: productId, quantity });
 		} else {
-			cart[productIndex].quantity += quantity;
+			// Nếu sản phẩm đã có trong giỏ hàng rồi mà ấn mua thêm thì cập nhật lại quantity
+			console.log(cart);
+			cart.products[productIndex].quantity += quantity;
 		}
+		cart.totalPrice += product.price * quantity;
+		console.log(cart);
+		await cart.save();
+		return res.status(200).json({
+			message: "Add to cart successfully",
+			cart,
+		});
 	} catch (error) {
 		next(error);
 	}
--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,15 +1,21 @@
-import { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const CartItem = new Schema({
-	product: { type: Schema.Types.ObjectId, ref: "Product" },
+const CartItem = new mongoose.Schema({
+	product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
 	quantity: Number,
 });
 
-const cartSchema = new Schema({
-	userId: { type: Schema.Types.ObjectId, ref: "User" },
-	products: [CartItem],
-	totalPrice: Number,
-});
+const cartSchema = new mongoose.Schema(
+	{
+		userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+		products: [CartItem],
+		totalPrice: Number,
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+	}
+);
 
 export default mongoose.model("Cart", cartSchema);
 
